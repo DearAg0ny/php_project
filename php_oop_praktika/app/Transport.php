6cd590e017ea5f8e4be113fb8d9764286018b201
Deimantas <116707288+DearAg0ny@users.noreply.github.com>
2023-02-01 09:01:25 +0200

    Update Transport.php

--- php_oop_praktika/app/Transport.php
+++ php_oop_praktika/app/Transport.php
@@ -13,17 +13,25 @@ class Transport
     protected $seat;
     protected $description;
 
+    /* Change model @param string $model */
     public function changeModel($modelName){
         $this->modelName = $modelName;
     }
+
+    /* Change make @param string $model */
     public function changeMake($make){
         $this->make = $make;
     }
+
+    /* Constructor */
     public function __construct($modelName, $make){
         $this->id = rand(5,100);
         $this->modelName = $modelName;
         $this->make = $make;
     }
+
+    /* Get all info about transport
+    @return Array */
     public function getInfo(){
         return [
             $this->modelName,
@@ -35,6 +43,8 @@ class Transport
             $this->description
         ];
     }
+
+    /* Setters */
     public function setPrice($price)
     {
         $this->price = $price;
@@ -54,9 +64,14 @@ class Transport
     public function setDescription($description){
         $this->description = $description;
     }
+
+    /* Display Transport info in HTML list */
     public function generateItemList($data){
         echo "<ul>";
         foreach ($data as $item){
+            if(is_null($item)){
+                
+            }else
             echo "<li>$item</li>";
         }
         echo "</ul>";
